b509553a0d484cfa9c0178e30c5b13674de28c51
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-10-31 17:46:46 -0300

    Adicionei o teste de erro de tipo

--- modulo3/exercicios/Categoria1/test_Remocao_Condicional_com_While.py
+++ modulo3/exercicios/Categoria1/test_Remocao_Condicional_com_While.py
@@ -5,3 +5,7 @@ import pytest
 def test_remocao():
     verificar=['abacate','abelha','indio','fone']
     assert sem_a(verificar)==['indio','fone']
+    
+def test_erro_de_tipo():
+    with pytest.raises(TypeError,match='Erro'):
+        sem_a(('abacate','abelha','indio','fone'))
