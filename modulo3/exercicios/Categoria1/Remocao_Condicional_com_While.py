51f14c9dbf655714f8b563250b20d093be35a2fb
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-10-31 17:46:31 -0300

    Adicionei erro de tipo

--- modulo3/exercicios/Categoria1/Remocao_Condicional_com_While.py
+++ modulo3/exercicios/Categoria1/Remocao_Condicional_com_While.py
@@ -1,15 +1,18 @@
-def sem_a(lista):
-    indice=0
+def sem_a(lista:list)->list:
+    if isinstance(lista,list):
+        indice=0
 
-    while len(lista)>indice:
+        while len(lista)>indice:
 
-        for i in lista:
-            
-            if 'a' in i:
-                lista.remove(i)
-            else:
-                indice+=1
-    return lista
+            for i in lista:
+                
+                if 'a' in i:
+                    lista.remove(i)
+                else:
+                    indice+=1
+        return lista
+    else:
+        raise TypeError('Erro')
 lista=['a','casa','é','bonita','banana','mouse','filme']
 
 a=sem_a(lista)
