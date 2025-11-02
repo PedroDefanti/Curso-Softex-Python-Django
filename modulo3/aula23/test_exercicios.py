811135eda4f9418883d049ed121ac591c613409c
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-11-02 17:04:01 -0300

    novas adições ao gitgnore,exerciciso para fazer dps

--- modulo3/aula23/test_exercicios.py
+++
@@ -1,8 +0,0 @@
-from exercicios import criacao,contar_letras
-
-
-def test_criacao():
-    assert criacao([1,2,3,4,5],[5,4,8,9])=={1,2,3,4,5,8,9}
-
-def test_contar():
-    assert contar_letras('banana')==  'A frase banana contém: CONSOANTES: 3 VOGAIS: 3'
