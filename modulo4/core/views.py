660178d59c806c14020d1c489aafcaeaa65c3bae
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-05 09:44:45 -0300

    mundaças do django

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('<h1> Olá, Mundo! </h1>')
-
-def frase(request):
-    return HttpResponse('<h1> Nova frase <b> aaaaaaa </b>')
 
