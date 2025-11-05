4b8bfbe79d1916044cb2078dd9bf20718565f6f3
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-05 11:36:26 -0300

    apredendo Django parte 2

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira pagina Django!</h1>")
+    context={
+        'nome_usuario':'Junior',
+        'tecnologias':['Python','Django','HTML','CSS']
+    }
+    return render(request,'home.html',context)
 
-
+def call(request):
+    return HttpResponse("<h1>Chamada por aqui.</h1>")
