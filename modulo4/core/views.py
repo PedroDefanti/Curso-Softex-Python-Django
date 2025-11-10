df7db8afd02e6c01eeffbfa0242439ed6c69a77e
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-10 11:34:29 -0300

    colocando o todas as tarefas e todas as exucucoes

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tarefa
+from .models import Execucacao
+
 
 
 # Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    todas_as_execucoes = Execucacao.objects.all()
     #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira pagina Django!</h1>")
     context={
         'nome_usuario':'Junior',
-        'tecnologias':['Python','Django','HTML','CSS']
+        'tecnologias':['Python','Django','HTML','CSS'],
+        'tarefas':todas_as_tarefas,
+        'execuções': todas_as_execucoes
     }
     return render(request,'home.html',context)
 
-def call(request):
-    return HttpResponse("<h1>Chamada por aqui.</h1>")
