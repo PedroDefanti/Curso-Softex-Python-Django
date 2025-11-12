de309c6ac134048668da75011b45e18c5a471008
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-12 11:28:23 -0300

    adicionando o forms

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Tarefa
 from .models import Execucacao
+from .forms import TarefaForm
 
 
 
 # Create your views here.
 def home(request):
+    if request.method=='POST':
+        form=TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=TarefaForm()
     todas_as_tarefas = Tarefa.objects.all()
     todas_as_execucoes = Execucacao.objects.all()
     #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira pagina Django!</h1>")
@@ -13,7 +21,8 @@ def home(request):
         'nome_usuario':'Junior',
         'tecnologias':['Python','Django','HTML','CSS'],
         'tarefas':todas_as_tarefas,
-        'execuções': todas_as_execucoes
+        'execuções': todas_as_execucoes,
+        'form':form,
     }
     return render(request,'home.html',context)
 
