b6674ad05e50c280d6a9e9fc1a1afda2535d676f
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-17 11:18:26 -0300

    adicionando as funções de concluir e deletar

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Tarefa
 from .models import Execucacao
 from .forms import TarefaForm
@@ -25,4 +25,19 @@ def home(request):
         'form':form,
     }
     return render(request,'home.html',context)
+
+
+def concluir_tarefa(request,pk):
+    tarefa = get_object_or_404(Tarefa,pk=pk)
+    if request.method == 'POST':
+        tarefa.concluida = True
+        tarefa.save()
+    return redirect('home')
+
+
+def deletar_tarefa(request,pk):
+    tarefa = get_object_or_404(Tarefa,pk=pk)
+    if request.method == 'POST':
+        tarefa.delete()
+    return redirect('home')
 
