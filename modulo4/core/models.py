b35f0336a1cbc01b022ebcd7a80f5b1c657473ad
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-19 11:39:43 -0300

    adicionei a coluna user

--- modulo4/core/models.py
+++ modulo4/core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Tarefa(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     concluida = models.BooleanField(default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.titulo
