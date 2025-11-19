1adf419a6c56df22e8cafbfa71710e8bb2b3fc15
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2025-11-19 11:40:41 -0300

    adicionei o decorator

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -2,42 +2,67 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Tarefa
 from .models import Execucacao
 from .forms import TarefaForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 
-
+@login_required
 # Create your views here.
 def home(request):
-    if request.method=='POST':
-        form=TarefaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form=TarefaForm()
-    todas_as_tarefas = Tarefa.objects.all()
-    todas_as_execucoes = Execucacao.objects.all()
-    #return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira pagina Django!</h1>")
-    context={
-        'nome_usuario':'Junior',
-        'tecnologias':['Python','Django','HTML','CSS'],
-        'tarefas':todas_as_tarefas,
-        'execuções': todas_as_execucoes,
-        'form':form,
-    }
-    return render(request,'home.html',context)
+ if request.method == 'POST':
+    form = TarefaForm(request.POST)
+ if form.is_valid():
+
+    tarefa = form.save(commit=False)
+ 
+    tarefa.user = request.user
+
+    tarefa.save()
+    return redirect('home')
+ else:
+    form = TarefaForm()
+
+ todas_as_tarefas = Tarefa.objects.filter(user=request.user).order_by('criado_em')
+
+ context = {
+ 'nome_usuario': request.user.username, 
+ 'tecnologias': ['Autenticação', 'ForeignKey', 'Login'],
+ 'tarefas': todas_as_tarefas,
+ 'form': form,
+ }
+ return render(request, 'home.html', context)
 
 
-def concluir_tarefa(request,pk):
-    tarefa = get_object_or_404(Tarefa,pk=pk)
+
+@login_required
+def concluir_tarefa(request, pk):
+ # 2. Modifique o 'get_object_or_404'
+ # Busque a Tarefa pela 'pk' E ONDE o 'user' é o 'request.user'
+    tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
     if request.method == 'POST':
         tarefa.concluida = True
-        tarefa.save()
-    return redirect('home')
-
-
-def deletar_tarefa(request,pk):
-    tarefa = get_object_or_404(Tarefa,pk=pk)
+        tarefa.save() # Não se esqueça de salvar!
+        return redirect('home')
+    
+@login_required
+def deletar_tarefa(request, pk):
+ # 3. Faça o mesmo filtro de segurança aqui
+    tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
     if request.method == 'POST':
         tarefa.delete()
-    return redirect('home')
+        return redirect('home')
+
+def register(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request,user)
+        return redirect('home')
+    else:
+        form = UserCreationForm()
+    context={'form':form}
+    return render(request,'register.html', context)
+        
 
